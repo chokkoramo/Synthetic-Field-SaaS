792f43612dcb3aedfc9c95207b5a14c988b2a4b5
JuanCaaa <perdomojuank300@gmail.com>
2026-06-15 15:00:06 -0500

    chore: Initial commit

---
+++ src/main/java/com/juank/synthetic_field_saas/SyntheticFieldSaasApplication.java
@@ -0,0 +1,13 @@
+package com.juank.synthetic_field_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntheticFieldSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntheticFieldSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juank/synthetic_field_saas/SyntheticFieldSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juank.synthetic_field_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntheticFieldSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
